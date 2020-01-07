d55dfef3364daa286f2fe504ff47d42af942fb30
omerfbas <omerbasdev@gmail.com>
2020-01-07 23:40:16 +0300

    model and controller

---
+++ OFB_NEWS/OFB_NEWS/Models/NewsDetailModel.cs
@@ -0,0 +1,19 @@
+﻿using OFB_NEWS.Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OFB_NEWS.Models
+{
+    public class NewsDetailModel
+    {
+        public NewsDetailModel()
+        {
+            Comments = new List<Comment>();
+        }
+        public Category Category { get; set; }
+        public News News { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
