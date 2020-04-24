07d7dabe22882e3a3c604750c5b2250a3dba7660
omerfbas <omerbasdev@gmail.com>
2020-04-24 23:50:25 +0300

    Data Manager

---
+++ OFB_NEWS/OFB_NEWS.Data/Managers/BaseManager.cs
@@ -0,0 +1,55 @@
+﻿using OFB_NEWS.Data.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OFB_NEWS.Data.Managers
+{
+    public class BaseManager<TEntity> where TEntity : class
+    {
+        NewsContext database;
+        public BaseManager()
+        {
+            database = new NewsContext();
+        }
+
+        public void AddManager(TEntity entity)
+        {
+            database.Set<TEntity>().Add(entity);
+            database.SaveChanges();
+        }
+
+        public void UpdateManager(TEntity entity)
+        {
+            database.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            database.SaveChanges();
+        }
+
+        public void DeleteManager(TEntity entity)
+        {
+
+            database.Set<TEntity>().Remove(entity);
+            database.SaveChanges();
+        }
+
+        public void DeleteIdManager(TEntity entity)
+        {
+
+            database.Entry(entity).State = System.Data.Entity.EntityState.Deleted;
+            database.SaveChanges();
+        }
+        public TEntity GetByIdManager(int id)
+        {
+
+            return database.Set<TEntity>().Find(id);
+        }
+
+        public List<TEntity> GetAllManager()
+        {
+
+            return database.Set<TEntity>().ToList();
+        }
+    }
+}
---
+++ OFB_NEWS/OFB_NEWS.Data/Managers/GalleryManager.cs
@@ -0,0 +1,13 @@
+﻿using OFB_NEWS.Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OFB_NEWS.Data.Managers
+{
+    public class GalleryManager : BaseManager<Gallery>
+    {
+    }
+}
